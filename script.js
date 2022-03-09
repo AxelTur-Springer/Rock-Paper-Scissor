34213939d5c0d7ffd64682a5749207fa5c8205e5
AxelTur-Springer <axel.turspringer@gmail.com>
2022-03-09 19:07:54 -0300

    finish

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const rulebox = document.getElementById("ruleBox");
 function btnCloseRules(){
     rulebox.style.display="none"
 }
-rulebox.style.display="none"
+
 
 
 let choice;
@@ -33,7 +33,15 @@ let score = 0;
         item.addEventListener('click', event => {
             
             document.getElementById("imagePc").src= ""
-           
+            document.getElementById("imagePc").style.width="7.2em"
+
+            document.getElementById("btnTxt").style.visibility = "hidden"
+            
+            const userImg = document.getElementById("imageUser")
+                userImg.style.animation = "none"
+            const pcImg = document.getElementById("imagePc")
+                pcImg.style.animation = "none"
+
             let winLosetxt= document.getElementById("winLose")
             winLosetxt.innerHTML=""
             
@@ -48,7 +56,8 @@ let score = 0;
                     let array = ["/assets/icon-rock.svg","/assets/icon-paper.svg","/assets/icon-scissors.svg"];
                     random = Math.floor((Math.random() * 3));
                     document.getElementById("imagePc").src=array[random];
-                  
+                    document.getElementById("imagePc").style.width="initial"
+
                     /* end random pc selection */ 
 
                     
@@ -56,30 +65,66 @@ let score = 0;
                    if(random === 0 && choice === "paper"){
                         score+=1 ,contador.innerHTML = score ;
                         winLosetxt.innerHTML="You win"
-                    }
-                    if(random === 2 && choice === "paper"){
-                        score-=1 ,contador.innerHTML = score;
-                        winLosetxt.innerHTML="You lose"
+                        userImg.style.animation = "winnerAnim 1s linear forwards"
+                        pcImg.style.animation = "loserAnim 1s linear forwards"
 
                     }
                     if(random === 0 && choice === "scissors"){
                         score-=1 ,contador.innerHTML = score
                         winLosetxt.innerHTML="You lose" 
+                        pcImg.style.animation = "winnerAnim 1s linear forwards"
+                        userImg.style.animation = "loserAnim 1s linear forwards"
+
                     }
                     if(random === 1 && choice === "scissors"){
                         score+=1 ,contador.innerHTML = score;
-                        winLosetxt.innerHTML="You win"  
+                        winLosetxt.innerHTML="You win"
+                        userImg.style.animation = "winnerAnim 1s linear forwards"
+                        pcImg.style.animation = "loserAnim 1s linear forwards"
+
                     }
                     if(random === 1 && choice === "rock"){
                         score-=1 ,contador.innerHTML = score;
                         winLosetxt.innerHTML="You lose" 
+                        pcImg.style.animation = "winnerAnim 1s linear forwards"
+                        userImg.style.animation = "loserAnim 1s linear forwards"
+
+                    }
+                    if(random === 2 && choice === "paper"){
+                        score-=1 ,contador.innerHTML = score;
+                        winLosetxt.innerHTML="You lose"
+                        pcImg.style.animation = "winnerAnim 1s linear forwards"
+                        userImg.style.animation = "loserAnim 1s linear forwards"
+
+
                     }
                     if(random === 2 && choice === "rock"){
                         score+=1 ,contador.innerHTML = score;
-                        winLosetxt.innerHTML="You win"  
+                        winLosetxt.innerHTML="You win" 
+                        userImg.style.animation = "winnerAnim 1s linear forwards"
+                        pcImg.style.animation = "loserAnim 1s linear forwards"
+
                     }
-                   // else if(){
-                     //   winLosetxt.innerHTML="Its a tie"}
+                    
+                  
+                    if( random === 1 && choice === "paper"||
+                        random === 2 && choice ==="scissors" ||
+                        random === 0 && choice === "rock"
+                    ){
+                     winLosetxt.innerHTML="Its a tie"
+                     pcImg.style.animation = "winnerAnim 1s linear forwards"
+                     userImg.style.animation = "winnerAnim 1s linear forwards"
+
+                    }
+                     
+                     setTimeout(function textbtn(){
+                        document.getElementById("btnTxt").style.visibility = "visible"
+                        document.getElementById("resultBox").style.animation = 
+                        "resultBoxAnimacion 0.5s linear forwards"
+
+                     },1000)
+                    
+                     
 
                 }, 1000);
             }
